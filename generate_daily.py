f236ef2db629ae0c9ed132a0e21a21788081eb55
Simon <sgazagnes@gmail.com>
2025-08-11 09:10:06 +0200

    Fixed news

--- generate_daily.py
+++ generate_daily.py
@@ -31,7 +31,7 @@ results = generate_daily_good_news(
     deepl_api_key=deepl_api_key,
     personality=None,
     max_articles=4,
-    generate_images=False
+    generate_images=True
 )
 
 # Save JSON with date and category
